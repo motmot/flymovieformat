80d94164a9f7bc43ee5507d3c270fb710ecf5060
astraw <astraw@9608957c-362e-0410-90bb-ddb82fccb57f>
2007-12-29 17:26:06 +0000

    moving to motmot namespace
    git-svn-id: https://code.astraw.com/motmot/trunk/FlyMovieFormat@346 9608957c-362e-0410-90bb-ddb82fccb57f

--- setup.py
+++ setup.py
@@ -1,30 +1,7 @@
-import sys
-from setuptools import setup
-import distutils.command.sdist
+from setuptools import setup, find_packages
 
-if 1:
-    kws=dict(
-      extras_require = {
-    'imops':      ['imops>=0.3.dev275'],
-    },
-
-      entry_points = {
-    'console_scripts': [
-    'fmf2bmps = FlyMovieFormat.fmf2bmps:main',
-    'fmf_collapse = FlyMovieFormat.fmf_collapse:main',
-    'fmf_info = FlyMovieFormat.fmf_info:main',
-    ],
-    'gui_scripts': [
-    'playfmf = FlyMovieFormat.playfmf:main [imops]',
-    'fmf_plottimestamps = FlyMovieFormat.fmf_plottimestamps:main',
-    ],
-    'FlyMovieFormat.exporter_plugins':[
-        'txt = FlyMovieFormat.playfmf:TxtFileSaverPlugin',
-        'fmf = FlyMovieFormat.playfmf:FmfFileSaverPlugin',
-        'image_sequence = FlyMovieFormat.playfmf:ImageSequenceSaverPlugin',
-        ],
-    },
-      )
+kws=dict(
+    )
 
 setup(name='FlyMovieFormat',
       description='support for .fmf files (part of the motmot camera packages)',
@@ -33,10 +10,23 @@ setup(name='FlyMovieFormat',
       author_email='strawman@astraw.com',
       license='BSD',
       url='http://code.astraw.com/projects/motmot',
-      packages = ['FlyMovieFormat'],
-      package_data = {'FlyMovieFormat':['playfmf.xrc',
-                                        'matplotlibrc',
-                                        'test_mono8.fmf',
-                                        'description.txt']},
-      **kws
+      namespace_packages = ['motmot'],
+      packages = find_packages(),
+      package_data = {'motmot.FlyMovieFormat':['playfmf.xrc',
+                                               'matplotlibrc',
+                                               'test_mono8.fmf',
+                                               'description.txt']},
+      entry_points = {
+    'console_scripts': ['fmf2bmps = motmot.FlyMovieFormat.fmf2bmps:main',
+                        'fmf_collapse = motmot.FlyMovieFormat.fmf_collapse:main',
+                        'fmf_info = motmot.FlyMovieFormat.fmf_info:main',
+                        ],
+    'gui_scripts': ['playfmf = motmot.FlyMovieFormat.playfmf:main',
+                    'fmf_plottimestamps = motmot.FlyMovieFormat.fmf_plottimestamps:main',
+                    ],
+    'FlyMovieFormat.exporter_plugins':['txt = motmot.FlyMovieFormat.playfmf:TxtFileSaverPlugin',
+                                       'fmf = motmot.FlyMovieFormat.playfmf:FmfFileSaverPlugin',
+                                       'image_sequence = motmot.FlyMovieFormat.playfmf:ImageSequenceSaverPlugin',
+                                       ],
+    },
       )
