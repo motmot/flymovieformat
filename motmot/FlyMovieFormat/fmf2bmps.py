59969a8b1797435f59777feb9785b4c12af60349
Andrew Straw <strawman@astraw.com>
2009-08-26 08:34:06 -0700

    make fmf2bmps much more flexible
    Original idea and implementation by Kristin Branson.

--- motmot/FlyMovieFormat/fmf2bmps.py
+++ motmot/FlyMovieFormat/fmf2bmps.py
@@ -1,31 +1,90 @@
 import os, sys
 import FlyMovieFormat
 import Image
+from optparse import OptionParser
+import motmot.imops.imops as imops
+import warnings
 
 def main():
-    try:
-        filename = sys.argv[1]
-    except:
-        print 'Usage: fmf2bmps fmf_filename'
-        sys.exit()
-        
-    path,ext = os.path.splitext(filename)
+    usage = """%prog FILE [options]
+
+Example:
+
+fmf2bmps myvideo.fmf --start=10 --stop=100 --extension=jpg --outdir=tmp
+"""
+
+    parser = OptionParser(usage)
+    parser.add_option('--start',type='int',default=0,help='first frame to save')
+    parser.add_option('--stop',type='int',default=-1,help='last frame to save')
+    parser.add_option('--extension',type='string',default='bmp',
+                      help='image extension (default: bmp)')
+    parser.add_option('--outdir',type='string',default=None,
+                      help='directory to save images (default: same as fmf)')
+    parser.add_option('--progress',action='store_true',default=False,
+                      help='show progress bar')
+    (options, args) = parser.parse_args()
+
+    if len(args)<1:
+        parser.print_help()
+        return
+
+    filename = args[0]
+    startframe = options.start
+    endframe = options.stop
+    imgformat = options.extension
+
+    base,ext = os.path.splitext(filename)
     if ext != '.fmf':
         print 'fmf_filename does not end in .fmf'
         sys.exit()
+    path,base = os.path.split(base)
+    if options.outdir is None:
+        outdir = path
+    else:
+        outdir = options.outdir
 
     fly_movie = FlyMovieFormat.FlyMovie(filename)
+    fmf_format = fly_movie.get_format()
     n_frames = fly_movie.get_n_frames()
+    if endframe < 0 or endframe >= n_frames:
+        endframe = n_frames - 1
 
-    for i in xrange(n_frames):
-        save_frame,timestamp = fly_movie.get_next_frame()
-        save_frame = save_frame[::-1,:] # flip for PIL
-        h,w=save_frame.shape
-        im = Image.fromstring('L',(w,h),save_frame.tostring())
-        f='%s_%08d.bmp'%(path,i)
-        print 'saving',f
+    fly_movie.seek(startframe)
+    n_frames = endframe+1-startframe
+    if options.progress:
+        import progressbar
+        widgets=['fmf2bmps', progressbar.Percentage(), ' ',
+                 progressbar.Bar(), ' ', progressbar.ETA()]
+        pbar=progressbar.ProgressBar(widgets=widgets,
+                                     maxval=n_frames).start()
+    else:
+        pbar = None
+
+    for count,i in enumerate(range(startframe,endframe+1)):
+        if pbar is not None:
+            pbar.update(count)
+        frame,timestamp = fly_movie.get_next_frame()
+
+        mono=False
+        if (fmf_format in ['RGB8','ARGB8','YUV411','YUV422'] or
+            fmf_format.startswith('MONO8:') or
+            fmf_format.startswith('MONO32f:')):
+            save_frame = imops.to_rgb8(fmf_format,frame)
+        else:
+            if fmf_format not in ['MONO8','MONO16']:
+                warnings.warn('converting unknown fmf format %s to mono'%(
+                    fmf_format,))
+            save_frame = imops.to_mono8(fmf_format,frame)
+            mono=True
+        h,w=save_frame.shape[:2]
+        if mono:
+            im = Image.fromstring('L',(w,h),save_frame.tostring())
+        else:
+            im = Image.fromstring('RGB',(w,h),save_frame.tostring())
+        f='%s_%08d.%s'%(os.path.join(outdir,base),i,imgformat)
         im.save(f)
+    if pbar is not None:
+        pbar.finish()
 
 if __name__=='__main__':
     main()
-    
