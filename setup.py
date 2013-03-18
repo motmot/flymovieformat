9b49ab30ef35689ead2faa6a662d74bc42152388
Andrew Straw <strawman@astraw.com>
2013-03-19 00:35:02 +0100

    bump version for release

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ fast, and a variety of formats (e.g. MONO8, YUV422) are supported.
 
 This is a subpackage of the motmot family of digital image utilities.
 """,
-      version='1.0',
+      version='1.1',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
       license='BSD',
