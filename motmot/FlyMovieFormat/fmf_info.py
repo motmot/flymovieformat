85254dcb67e2d93acd94285bd1d11b3755b35f6f
John Stowers <john.stowers@gmail.com>
2013-05-27 18:27:29 +0200

    fmf_info: print timestamps

--- motmot/FlyMovieFormat/fmf_info.py
+++ motmot/FlyMovieFormat/fmf_info.py
@@ -5,24 +5,18 @@ def main():
     try:
         filename1 = sys.argv[1]
     except:
-        print """Usage: fmf_info fmf_filename
-
-This program takes a set of .fmf files and saves the mean of all
-frames from all files as a .bmp image file.
-
-The files used are in the range fmf_filename1 <= FILE <= fmf_filename2.
-
-"""
+        print """Usage: fmf_info fmf_filename"""
         sys.exit()
 
     fly_movie = FlyMovieFormat.FlyMovie(filename1)
-    x = fly_movie.get_height
-    print '%s: %d frames, %d x %d pixels, %s format (%d bpp)'%(
+    ts = fly_movie.get_all_timestamps()
+    print '%s: %d frames, %d x %d pixels, %s format (%d bpp), timestamps range %.1f to %.1f'%(
         filename1,
         fly_movie.get_n_frames(),
         fly_movie.get_width(), fly_movie.get_height(),
         fly_movie.get_format(),
         fly_movie.get_bits_per_pixel(),
+        ts[0],ts[-1],
         )
 
 if __name__=='__main__':
