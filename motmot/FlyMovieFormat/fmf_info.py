963fa858ffb8f46649301ed051cabd4727c36b90
Andrew Straw <strawman@astraw.com>
2017-07-19 17:00:11 +0200

    fmf_info: python3 fixes and print more info

--- motmot/FlyMovieFormat/fmf_info.py
+++ motmot/FlyMovieFormat/fmf_info.py
@@ -1,23 +1,26 @@
-import sys
+from __future__ import print_function
+import sys, datetime
 import FlyMovieFormat
 
 def main():
     try:
         filename1 = sys.argv[1]
     except:
-        print """Usage: fmf_info fmf_filename"""
+        print("Usage: fmf_info fmf_filename")
         sys.exit()
 
     fly_movie = FlyMovieFormat.FlyMovie(filename1)
     ts = fly_movie.get_all_timestamps()
-    print '%s: %d frames, %d x %d pixels, %s format (%d bpp), timestamps range %.1f to %.1f'%(
+    dt0 = datetime.datetime.fromtimestamp(ts[0])
+    dt1 = datetime.datetime.fromtimestamp(ts[-1])
+    print('%s: %d frames, %d x %d pixels, %s format (%d bpp), timestamps range %.1f (%s) to %.1f (%s)'%(
         filename1,
         fly_movie.get_n_frames(),
         fly_movie.get_width(), fly_movie.get_height(),
         fly_movie.get_format(),
         fly_movie.get_bits_per_pixel(),
-        ts[0],ts[-1],
-        )
+        ts[0],dt0.isoformat(),ts[-1],dt1.isoformat(),
+        ))
 
 if __name__=='__main__':
     main()
