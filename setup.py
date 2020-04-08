f6ce6043a46e4070dff8d76eba92e0e2a433ec8f
Andrew Straw <strawman@astraw.com>
2020-04-08 12:41:57 +0200

    bump version for release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="motmot.FlyMovieFormat",
-    version="1.9",
+    version="1.10",
     description="support for .fmf (fly movie format) files",
     long_description=long_description,
     long_description_content_type="text/markdown",
